47089d004b25a857f52c303f679a2e48a80475a4
Karina da Mota <135737112+kjmota@users.noreply.github.com>
2023-11-07 09:54:09 -0300

    Primeiro commit java herança

---
+++ eclipse-workspace/java-heranca/src/LivroDigital.java
@@ -0,0 +1,25 @@
+
+public class LivroDigital extends Livro {
+
+	private int download;
+	private double tamanho;
+	
+	public int getDownload() {
+		return download;
+	}
+	
+	public void setDownload(int download) {
+		this.download = download;
+	}
+	
+	public double getTamanho() {
+		return tamanho;
+	}
+	
+	public void setTamanho(double tamanho) {
+		this.tamanho = tamanho;
+	}
+	
+	
+	
+}
---
+++ eclipse-workspace/java-heranca/src/LivroFisico.java
@@ -0,0 +1,24 @@
+
+public class LivroFisico extends Livro {
+
+	private int triagem;
+	private int peso;
+	
+	public int getTriagem() {
+		return triagem;
+	}
+	
+	public void setTriagem(int triagem) {
+		this.triagem = triagem;
+	}
+	
+	public int getPeso() {
+		return peso;
+	}
+	
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	
+	
+}
